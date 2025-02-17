f299ead2d0a3bc665cb63771005edad9aa9cb5e6
smd255 <sumida.shogo368@gmail.com>
2025-02-17 23:27:38 +0900

    auth POST OK

--- app/schemas/memo.py
+++ app/schemas/memo.py
@@ -37,3 +37,10 @@ class MemoSchema(InsertAndUpdateMemoSchema):
         description="メモを一意に識別するID番号。データベースで自動的に割り当てられます。",
         example=123,
     )
+
+    # メモを登録したユーザーのID
+    # user_id: int = Field(
+    #     ...,
+    #     description="メモを登録したユーザーのID",
+    #     example=123,
+    # )
